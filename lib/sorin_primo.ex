16ffcf157b969996a964d1838831860ea9bc2632
Casey Gibbs <caseygibbs@gmail.com>
2019-08-20 17:03:34 -0500

    Correct api_key assignment in sorin_primo.ex

--- lib/sorin_primo.ex
+++ lib/sorin_primo.ex
@@ -34,7 +34,7 @@ defmodule SorinPrimo do
       "&tab=#{Application.get_env(:sorin_primo, :tab)}" <>
       "&scope=#{Application.get_env(:sorin_primo, :scope)}"<>
       "&q=#{filters["search_by"] || "any"},contains,#{encoded_search_string}" <>
-      "&apikey=#{System.get_env("PRIMO_API_KEY")}" <>
+      "&apikey=#{Application.get_env(:sorin_primo, :api_key)}" <>
     (if (filters["item_type"] == "newspapers"), do:
       "&newspapersActive=true&newspapersSearch=true",
 	else: "" ) <>
